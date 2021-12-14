c6c021c22c8f2e1106abe9984cc296587e87ca47
humed <abc45171@gmail.com>
2021-12-14 16:54:11 +0300

    Models

---
+++ Model/accessories.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const accessory = mongoose.Schema({
+    accessoryName : {
+        type : String,
+        require : true,
+    }
+})
+
+
+const Accessory = mongoose.model('accessory',accessory)
+exports.Accessory = Accessory
